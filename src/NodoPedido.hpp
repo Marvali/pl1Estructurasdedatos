50b1b24e78c7f2608cca8b7de57c9f626e597ed3
Pablo Brañas <pablobranasdelasota@gmail.com>
2023-10-25 12:27:32 +0200

    Nodo pedido

---
+++ src/NodoPedido.hpp
@@ -0,0 +1,30 @@
+#ifndef NODOPEDIDO_HPP
+#define NODOPEDIDO_HPP
+#include <iostream>
+using namespace std;
+#include "pedido.h"
+
+class NodoPedido{
+private:
+    Pedido valor;
+    NodoPedido *siguiente
+    friend class Cola;
+    friend class cpedido;
+public:
+    NodoPedido(Pedido x, NodoPedido *sig = NULL);
+    ~NodoPedido();
+};
+
+typedef NodoPedido *pnodoPedido;
+
+#endif
+
+//Constructor de la clase nodo
+NodoPedido::NodoPedido(Pedido x, NodoPedido *sig){
+    valor = x;
+    siguiente = sig;
+}
+
+//Destructor de la clase nodo
+NodoPedido::~NodoPedido(){
+}
